1f57f5cb686e7321e334bb02b48bb0d6fd0e2931
Illia <79321689+illiaxpwnz@users.noreply.github.com>
2026-03-02 08:05:47 +0200

    Update tablecypher.py

--- tablecypher.py
+++ tablecypher.py
@@ -84,7 +84,7 @@ All art is at once surface and symbol. Those who go beneath the surface do so at
 Those who read the symbol do so at their peril. It is the spectator, and not life, that art really mirrors. 
 Diversity of opinion about a work of art shows that the work is new, complex, vital. 
 When critics disagree the artist is in accord with himself. We can forgive a man for making a useful thing as long as he does not admire it. 
-The only excuse for making a useless thing is that one admires it intensely. All art is quite useless.
+The only excuse for making a useless thing is that one admires it intensely. All art is quite useless
 """
 
 # Шифрування тексту з табличним шифром
